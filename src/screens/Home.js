5eebaafa47ff546ba168d03a6cb0ed31582cbd49
Pedro Bortolli <pedrovbortolli@gmail.com>
2020-05-17 13:40:10 -0300

    Initial commit

---
+++ src/screens/Home.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Home = () => {
+    return (
+        <Container>
+            LoR Home
+        </Container>
+    )
+}
+
+export default Home
+
+const Container = styled.div`
+    background-color: blue;
+    color: white;
+    font-size: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+`
