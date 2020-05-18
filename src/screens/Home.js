c993082c96e7f6ecef342fbe854679662b71d4df
Pedro Bortolli <pedrovbortolli@gmail.com>
2020-05-18 13:54:35 -0300

    Fix position-rectangles api call

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -13,16 +13,21 @@ const Home = () => {
         const search = async () => {
             console.log('searching for game')
             const gameData = await searchGame()
-            if (gameData.ok) setGameActive(isGameActive)
-            if (isGameActive) setGame({ found: true, data: gameData })
+            if (gameData.ok) setGame({ found: true, data: gameData.data })
         }
         search()
     }, game.found ? null : SEARCH_FREQUENCY)
 
+    console.log(game.data, game.data['PlayerName'])
+
     return (
         <Container>
             {game.found ?
-                <p>{game.data.PlayerName} vs {game.data.OpponentName}</p>
+                <Column>
+                    <div>{game.data.PlayerName}</div>
+                    <div>vs</div>
+                    <div>{game.data.OpponentName}</div>
+                </Column>
                 :
                 <p>Leave this screen open as you search for a match. Once the game starts your data will be automatically shown here</p>
             }
@@ -44,4 +49,9 @@ const Container = styled.div`
     > p {
         text-align: center;
     }
+`
+const Column = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 `
