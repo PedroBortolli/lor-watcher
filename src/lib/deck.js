492df79badb330dc35cb13b3eb7be880c7988079
Pedro Bortolli <pedrovbortolli@gmail.com>
2020-05-21 14:07:41 -0300

    Parse required information about decks winrates and render them under /decks

--- src/lib/deck.js
+++ src/lib/deck.js
@@ -2,8 +2,10 @@ export const getRegionsChampions = (cardsPlayed, set) => {
     let regions = {}, champions = {}
     cardsPlayed.forEach(cardPlayed => {
         const card = set.find(c => c.cardCode === cardPlayed.code)
-        regions[card.region] = true
-        if (card.supertype === 'Champion') champions[card.name] = true
+        if (card) {
+            regions[card.region] = true
+            if (card.supertype === 'Champion') champions[card.name] = true
+        }
     })
 
     return [Object.keys(regions), Object.keys(champions)]
--- src/screens/Decks.js
+++ src/screens/Decks.js
@@ -1,11 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import { hot } from 'react-hot-loader'
+import storage from 'electron-json-storage'
+import { getRegionsChampions } from '../lib/deck'
+import { getDeckCards } from '../lib/local'
+import { getCards } from '../api/api'
 
 const Decks = () => {
+    const [orderedRecords, setRecords] = useState([])
+    const [cardsSet, setCardsSet] = useState([])
+    useEffect(() => {
+        const getSet = async () => {
+            const cards = await getCards()
+            setCardsSet(cards)
+        }
+        storage.get('history', (err, history) => {
+            if (!err && history && history instanceof Array) {
+                let records = {}
+                history.forEach(match => {
+                    if (match.deck in records) records[match.deck][match.won ? 'wins' : 'losses']++
+                    else records[match.deck] = { wins: match.won ? 1 : 0, losses: match.won ? 0 : 1}
+                })
+                setRecords(Object.keys(records).reduce((tot, current) => {
+                    tot.push({...records[current], code: current})
+                    return tot
+                }, []).sort((a, b) => a.wins < b.wins ? 1 : -1))
+            }
+            getSet()
+        })
+    }, [])
     return (
         <Container>
-            Decks
+            {orderedRecords.length > 0 && cardsSet.length > 0 && orderedRecords.map(record => {
+                if (record.code) {
+                    const [regions, champions] = getRegionsChampions(getDeckCards(record.code), cardsSet)
+                    //console.log(regions, champions)
+                    return <Card>
+                        <div>{record.code.substr(0, 10) + '...'}</div>
+                        <div>{record.wins}/{record.losses}</div>
+                        <div>{regions.map(region => <span>{region}</span>)}</div>
+                    </Card>
+                }
+                return null
+            })}
         </Container>
     )
 }
@@ -13,11 +50,17 @@ const Decks = () => {
 export default hot(module)(Decks)
 
 const Container = styled.div`
-    background: #590f17;
-    height: 100%;
+    height: fill-available;
+    width: 100%;
     color: white;
     display: flex;
-    justify-content: center;
+    flex-direction: column;
     align-items: center;
-    font-size: 20px;
+    font-size: 16px;
+`
+const Card = styled.div`
+    border: 2px solid #132b66;
+    border-radius: 4px;
+    width: fill-available;
+    margin: 0 8px 32px;
 `
