45210498f41a958e0315b24f373b422609916a43
Pedro Bortolli <pedrovbortolli@gmail.com>
2020-05-23 14:10:26 -0300

    Hide menu bar

--- src/main.js
+++ src/main.js
@@ -27,6 +27,8 @@ const createWindow = () => {
   // and load the index.html of the app.
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
   mainWindow.setAlwaysOnTop(true, 'screen');
+  mainWindow.setMenuBarVisibility(false);
+  mainWindow.removeMenu();
 
   // Open the DevTools.
   //mainWindow.webContents.openDevTools();
